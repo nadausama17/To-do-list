dca447ab6844da2e78731c8261326f0487d24080
nada <nadaua2@gmail.com>
2022-08-12 14:57:20 +0200

    add basic files

---
+++ JS/scripts.js
@@ -0,0 +1,11 @@
+//status is set to false to refer that task is not completed
+//favourite is set to false to refer that task is not in user's favourites
+const task = [
+    {key:"id", value:Date.now()},
+    {key:"name", value:null},
+    {key:"details", value:null},
+    {key:"deadline", value:null},
+    {key:"status", value:false},
+    {key:"favourite", value:false}
+];
+
